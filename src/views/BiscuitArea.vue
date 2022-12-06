f7c2b7b4cc04a7032048ed805ad8ad9c85d639f2
pulcherriman <tsukasawa_agu@yahoo.co.jp>
2022-12-07 00:32:38 +0900

    クッキーが自動で増えるようになった
    Co-authored-by: Kakeru Ito <makecir@users.noreply.github.com>
    Co-authored-by: sugitanishi <sugitanishi@users.noreply.github.com>

--- src/views/BiscuitArea.vue
+++ src/views/BiscuitArea.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="flex flex-col items-center justify-evenly bg-yellow-50">
-		<p class="text-4xl">{{ saveData.biscuits }} biscuit</p>
+		<div class='w-full bg-slate-500 bg-opacity-40 flex flex-col items-center justify-evenly py-3'>
+			<p class="text-xl py-5">{{bakeryName}}'s bakery</p> 
+			<p class="text-4xl py-1">{{ Math.floor(saveData.biscuits) }} biscuits</p>
+			<p class="text-base py-1">per second: {{ saveData.biscuitsPerSecond }}</p>  
+		</div>
 		<div class="w-3/4">
 			<img @click="biscuitIncrement" src="@/assets/click_biscuit.svg"/>
 		</div>
@@ -14,12 +18,18 @@
 <script setup lang="ts">
 
 import { useStatusStore } from '@/stores/status'
+import { useIntervalFn } from '@vueuse/core'
 const saveDataStore = useStatusStore();
 const saveData = saveDataStore.saveData;
-
 const biscuitIncrement = function(){
-	saveData.biscuits++;
-
+	saveData.biscuits+=saveData.biscuitsPerClick;
 }
+
+const interval = 100;
+const bakeryName = "BigCrunch";
+const { pause, resume, isActive } = useIntervalFn(()=>{
+	saveData.biscuits += saveData.biscuitsPerSecond * interval / 1000;
+}, interval);
+
 
 </script>
